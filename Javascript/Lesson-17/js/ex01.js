ddb330fc867a9a09f68d6086e980aa33917feb8e
tungv62333 <tungv62333@gmail.com>
2024-05-22 10:50:01 +0700

    update JS-1

--- Javascript/Lesson-17/js/ex01.js
+++ Javascript/Lesson-17/js/ex01.js
@@ -1,40 +1,43 @@
 var a, b, c, s, temp
 // Bài 1
-console.log('Bài 1: Hoán vị 2 số')
+document.write(`<br>Bài 1: Hoán vị 2 số`)
 a = 5
 b = 12
-console.log(`a = ${a}, b = ${b}`)
+document.write(`<br>a = ${a}, b = ${b}`)
 a = a - b
 b = b + a
 a = b - a
-console.log(`Sau khi hoán vị: a = ${a}, b = ${b}`)
+document.write(`<br>Sau khi hoán vị: a = ${a}, b = ${b}`)
 
 // Bài 2
-console.log('Bài 2: Thực hiện phép toán')
+document.write(`<br>Bài 2: Thực hiện phép toán`)
 s = 10 + 20 + 5**10 / 2
-console.log(`s = 10 + 20 + 5^10 / 2 = ${s}`)
+document.write(`<br>s = 10 + 20 + 5^10 / 2 = ${s}`)
 
 // Bài 3
-console.log('Bài 3: Tìm số lớn nhất')
+document.write(`<br>Bài 3: Tìm số lớn nhất`)
 a = 2
 b = 1
 c = 4
+document.write(`<br>a = ${a}, b = ${b}, c = ${c}`)
 a = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-console.log(`Số lớn nhất là: ${a}`)
+document.write(`<br>Số lớn nhất là: ${a}`)
 
 // Bài 4
-console.log('Bài 4: Kiểm tra số cùng dấu')
+document.write(`<br>Bài 4: Kiểm tra số cùng dấu`)
 a = -1
 b = 5
-a * b > 0 ? console.log('Hai số cùng dấu') : console.log('Hai số khác dấu')
+document.write(`<br>a = ${a}, b = ${b}`)
+a * b > 0 ? document.write(`<br>Hai số cùng dấu`) : document.write(`<br>Hai số khác dấu`)
 
 // Bài 5
-console.log('Bài 5: Sắp xếp 3 số')
+document.write(`<br>Bài 5: Sắp xếp 3 số`)
 a = 6
 b = 2
 c = 3
+document.write(`<br>a = ${a}, b = ${b}, c = ${c}`)
 var first, mid, last
 first = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
 last = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 mid = (a + b + c) - first - last;
-console.log(`Ba số sau khi sắp xếp tăng dần là : ${first},${mid},${last} `)
+document.write(`<br>Ba số sau khi sắp xếp tăng dần là : ${first},${mid},${last} `)
